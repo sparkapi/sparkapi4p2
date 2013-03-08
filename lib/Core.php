48a7d79e79db730bf8500ff9f0136caf8a8eb84b
root <root@clancy.fbsdata.com>
2013-03-08 13:21:44 -0600

    Removed url encoding of entire URI

--- lib/Core.php
+++ lib/Core.php
@@ -830,7 +830,6 @@ class SparkAPI_Core {
 		if ($post_data) {
 			$post_data = $this->make_sendable_body($post_data);
 		}
-		$service = rawurlencode($service);
 		$service = trim($service, "/ ");
 
 		return $this->MakeAPICall($method, $service, $cache_time, $params, $post_data, $a_retry);
