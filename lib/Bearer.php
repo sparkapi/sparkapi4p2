3083b09841a443188ae46ce675cf482d3e563fa1
Tim Kramer <tim@thethinkery.net>
2018-01-12 21:39:52 -0800

    Bearer Token
    Use with OIDC or Bearer token

---
+++ lib/Bearer.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App;
+
+
+use SparkAPI_AuthInterface;
+use SparkAPI_Core;
+
+class Bearer extends SparkAPI_Core implements SparkAPI_AuthInterface
+{
+    public $access_token;
+
+    public function __construct($access_token)
+    {
+        $this->access_token = $access_token;
+        $this->force_https = true;
+        parent::__construct();
+    }
+
+    function sign_request($request) {
+        $this->SetHeader('Authorization', 'Bearer '. $this->access_token);
+        $this->SetHeader('X-SparkApi-User-Agent', 'Thinkery');
+
+        // reload headers into request
+        $request['headers'] = $this->headers;
+        $request['query_string'] = http_build_query($request['params']);
+        $request['cacheable_query_string'] = $request['query_string'];
+
+        return $request;
+    }
+}
